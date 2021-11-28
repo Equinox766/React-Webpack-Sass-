819aff7e39c4bf4ce74cfca3128b787550a341e4
Equinox766 <Equinox766@gmail.com>
2021-11-28 19:44:44 -0300

    Primer Commit del proyecto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+    return(
+        <div className="app">
+            <h1>React + Webpack + Sass</h1>
+            <p>Mi primer proyecto de React con Webpack</p>
+        </div>
+    )
+}
+export default App;
