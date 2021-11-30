76e108943bde3d3fa6438fe514e92217331c81bf
Equinox766 <Equinox766@gmail.com>
2021-11-29 23:23:28 -0300

    Proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import "./App.scss";
-
-function App() {
-    return(
-        <div className="app">
-            <h1>React + Webpack + Sass</h1>
-            <p>Mi primer proyecto de React con Webpack</p>
-        </div>
-    )
+import React, { Fragment } from 'react';
+import Header from "./components/Header";
+import Characters from './components/Characters';
+import './main.sass';
+import './components/Header.css';
+const App = () => {
+    return (
+        <Fragment>
+            <Header
+                title="Rick and Morty API"
+            />
+            <Characters />
+        </Fragment>
+    );
 }
+
 export default App;
